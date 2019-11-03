443fbf8778b54be7c6c046a2d62c22283acb3793
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-11-03 13:54:51 -0500

    general update

--- tts.js
+++ tts.js
@@ -1,25 +1,44 @@
-"use strict"
-
 function tts(){
 
   // Remove non-digit characters & prepare arrays
   const wp = document.getElementById('wp').value
-  var clean = /\d/g
+  const clean = /\d/g
   var wp1 = wp.match(clean)
   var wp2 = wp1.split("")
-  if (wp2 === (null || "0" || "1")){document.write("ERROR - NO PIN ENTERED")}
-  // Just having 0 or 1 only is as useless as having no PIN at all.
-  var sl = (Math.round(Math.sqrt(wp2)) + 1)
+  if (wp2 === (null || "0" || "1")){
+    // Just having 0 or 1 only is as useless as having no PIN at all.
+    document.write("ERROR - NO PIN ENTERED")
+    return
+  }
+  const sl = (Math.round(Math.sqrt(wp2)) + 1)
   // The "+ 1" is for any leftover characters.
-  const res = []
+  // If there are 16 characters, for example, then there will be four rows/arrays.
+  var res = []
   for (i = 0; i < (sl); i++){res.push([])}
+  const ta = document.getElementById('ta').value
+  var ta1 = ta.replace("",/\s/g)
+  var txt = ta1.split("")
+
+  function count(number){
+    for (i = 0; i < number; i++) {
+      do {
+        let x = wp2.pop()
+        res[i].push(x)
+      } while (res[i].length < sl);
+    }
+  }
 
   const e = document.getElementById('e').checked
   if (e === true){
+
     // Wrap
+    for (i = 0; i < wp2.length; i++) {count(wp2[i])}
 
   } else {
+
     // Unwrap
+    wp2.reverse()
+    for (i = 0; i < wp2.length; i++) {count(wp2[i])}
 
   }
 
