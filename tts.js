e8e1ebed2336066cd4f6d8dedea21f3681420635
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-11-03 23:36:05 -0500

    Update tts.js

--- tts.js
+++ tts.js
@@ -1,6 +1,6 @@
 function tts(){
 
-  // Remove non-digit characters & prepare arrays
+  // Remove non-digit characters
   const wp = document.getElementById('wp').value
   const clean = /\d/g
   var wp1 = wp.match(clean)
@@ -10,38 +10,45 @@ function tts(){
     document.write("ERROR - NO PIN ENTERED")
     return
   }
+
+  // Prepare arrays
   const sl = (Math.round(Math.sqrt(wp2)) + 1)
   // The "+ 1" is for any leftover characters.
   // If there are 16 characters, for example, then there will be four rows/arrays.
   var res = []
-  for (i = 0; i < (sl); i++){res.push([])}
+  for (i = 0; i < sl; i++){res.push([])}
   const ta = document.getElementById('ta').value
-  var ta1 = ta.replace("",/\s/g)
+  var ta1 = ta.replace(" ","")
   var txt = ta1.split("")
-
-  function count(number){
-    for (i = 0; i < number; i++) {
-      do {
-        let x = wp2.pop()
-        res[i].push(x)
-      } while (res[i].length < sl);
-    }
+  for (i = 0; i < number; i++){
+    do {
+      let x = txt.pop()
+      res[i].push(x)
+    } while ((res[i].length < sl) || (res[i].length !== 0));
   }
 
+  function count(number){
+  }
+
+  // Encryption/Decryption
   const e = document.getElementById('e').checked
   if (e === true){
 
     // Wrap
-    for (i = 0; i < wp2.length; i++) {count(wp2[i])}
+    txt.reverse()
+    // Reverses txt in wrap because ".pop()" removes last item.
+    wp2.foreach(count())
 
   } else {
 
     // Unwrap
     wp2.reverse()
-    for (i = 0; i < wp2.length; i++) {count(wp2[i])}
+    wp2.foreach(count())
 
   }
 
+  // Remove possible empty array and display results.
+  if (res[res.length] === 0){res[res.length] = null}
   document.write(res)
 
 }
