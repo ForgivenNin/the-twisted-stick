2be75d49e962532eaf3d1000d70fb3cc2fa5454d
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-11-04 23:46:39 -0500

    begin count function

--- tts.js
+++ tts.js
@@ -8,11 +8,9 @@ function tts () {
   var ta = document.getElementById('ta').value
   var ta1 = ta.replace(/\s/g, '')
   var txt = ta1.split('')
-  console.log(txt.length)
   txt.reverse()
   // Evening out text into a square-like arrangement.
   var sl = (Math.round(Math.sqrt(txt.length)))
-  console.log(sl * 2)
   // The "* 2" is for the destined set of arrays.
   for (i = 0; i < (sl * 2); i++) {
     res.push([])
@@ -20,13 +18,18 @@ function tts () {
   for (i = 0; i < res.length; i++) {
     do {
       var x = txt.pop()
-      console.log('pop text ' + i + " " + x);
       res[i].push(x)
     } while (res[i].length < sl)
   }
 
   function count (number) {
+    var y
     for (var i = 0; i < number; i++) {
+      if ((i < res.length) || (res[i] !== null) || (res[i + sl].length < sl)) {
+        y = res[i].shift()
+        console.log(y)
+        res[i + sl].unshift(y)
+      }
     }
   }
 
@@ -34,7 +37,6 @@ function tts () {
   var e = document.getElementById('e').checked
   if (e === true) {
     // Wrap
-    // Reverses txt in wrap because ".pop()" removes last item.
     wp2.forEach(count)
   } else {
     // Unwrap
@@ -43,7 +45,7 @@ function tts () {
     wp2.forEach(count)
   }
 
-  // Remove possible empty array and display results.
+  // Remove empty arrays & display results.
   document.getElementById('p').innerText = res
   console.log(res);
 }
