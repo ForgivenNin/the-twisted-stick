1f60e29cf1bdb1264daa88ff54ab7fb99b424048
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-11-04 20:38:11 -0500

    Add files via upload

--- tts.js
+++ tts.js
@@ -1,53 +1,49 @@
 function tts () {
-  // Remove non-digit characters
+  // Remove non-digit characters.
   var wp = document.getElementById('wp').value
-  var clean = /\d/g
-  var wp1 = wp.match(clean)
-  var wp2 = wp1.split('')
-  if (wp2 === (null || '0' || '0')) {
-    // Just having 0 or 1 only is as useless as having no PIN at all.
-    document.getElementById('p').innerText = 'ERROR - NO PIN ENTERED'
-    return
-  }
-  // Prepare arrays
+  var wp2 = wp.split('')
+  // Prepare arrays.
   var i = 0
-  var sl = ((Math.round(Math.sqrt(wp2)) + 1) * 2)
-  // The "+ 1" is for any leftover characters.
-  // If there are 16 characters, for example, then there will be four rows/arrays.
   var res = []
-  for (i = 0; i < sl; i++) {
+  var ta = document.getElementById('ta').value
+  var ta1 = ta.replace(/\s/g, '')
+  var txt = ta1.split('')
+  console.log(txt.length)
+  txt.reverse()
+  // Evening out text into a square-like arrangement.
+  var sl = (Math.round(Math.sqrt(txt.length)))
+  console.log(sl * 2)
+  // The "* 2" is for the destined set of arrays.
+  for (i = 0; i < (sl * 2); i++) {
     res.push([])
   }
-  var ta = document.getElementById('ta').value
-  var ta1 = ta.replace(' ', '')
-  var txt = ta1.split('')
-  // Sort characters
   for (i = 0; i < res.length; i++) {
     do {
-      const x = txt.pop()
+      var x = txt.pop()
+      console.log('pop text ' + i + " " + x);
       res[i].push(x)
     } while (res[i].length < sl)
   }
-  
+
   function count (number) {
+    for (var i = 0; i < number; i++) {
+    }
   }
-  
+
   // Encryption/Decryption
   var e = document.getElementById('e').checked
   if (e === true) {
     // Wrap
-    txt.reverse()
     // Reverses txt in wrap because ".pop()" removes last item.
-    wp2.foreach(count())
+    wp2.forEach(count)
   } else {
     // Unwrap
+    txt.reverse()
     wp2.reverse()
-    wp2.foreach(count())
+    wp2.forEach(count)
   }
 
   // Remove possible empty array and display results.
-  if (res[res.length] === 0) {
-    res[res.length] = null
-  }
   document.getElementById('p').innerText = res
+  console.log(res);
 }
