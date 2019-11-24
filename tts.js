8e46ad1e248f2a405c8dd22946085672de7e1bec
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-11-24 10:21:32 -0500

    Update tts.js

--- tts.js
+++ tts.js
@@ -3,27 +3,41 @@ function tts () {
   var pin = document.getElementById('wp').value
   var pin2 = pin.replace(/\D/g,'')
   var pin3 = pin2.split('')
+  var pin4 = pin3.map(function (x) { 
+    return parseInt(x, 10); 
+  })
 
   var txt = document.getElementById('ta').value
   var txt1 = txt.replace(/\s/g, '')
   var txt2 = txt1.split('')
+  var txtl = txt2.length
 
   // Cut
   var a = []
+  var c = []
+  var d = true
   do {
     // Pin loop
-    for (var i = 0; i < pin3.length; i++) {
-      if (pin3[i] === undefined) {
-        break
-      }
+    for (var i = 0; i < pin4.length; i++) {
+      c.push(pin4[i])
+      console.log(c)
       var b = []
       // Pin # (amount) loop)
-      for (var j = 0; j < pin3[i]; j++) {
+      for (var j = 0; j < pin4[i]; j++) {
         b += txt2.shift()
+        var e = c.reduce((a, b) => a + b, 0)
+        console.log(e)
+        if (e > txtl) {
+          d = false
+          break
+        }
+      }
+      if (d === false) {
+          break
       }
       a.push(b)
     }
   // Leftover loop
-} while (a.length < txt2.length)
+  } while (d === true)
   document.getElementById('p').innerHTML = a
 }
