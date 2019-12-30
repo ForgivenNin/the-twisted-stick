288f366810b10edaaf34cda37d1bc8e3e48b6c97
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-12-29 19:11:45 -0500

    Create tts.pl

---
+++ tts.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+use strict;
+
+# TTS.PL - Built to cooperate with asemicrypt
+
+my $txt0 
+my $txt1 =~ s/\s/\b/g
+my $txt =~ uc $txt1
+my $txt23
+my $txt33 =~ split str/g $txt23
+my $txt2
+my $txt3
+my $txt4
+
+open('PIN','<', "PIN.txt")
+my $PIN = PIN;
+close('PIN')
+unlink "PIN.txt
+my $pin1
+my $pin2
+my $pin3
+my $pin4
+my $pin5
