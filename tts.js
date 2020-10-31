6c319adf4132b52b219c61530807216d48a88598
fr33roam3r <46826941+fr33roam3r@users.noreply.github.com>
2020-10-31 16:08:44 -0400

    descriptive variables part 1

--- tts.js
+++ tts.js
@@ -1,89 +1,109 @@
-function tts (mode) {
-  // Prepare text
-  var txt0 = document.getElementById('ta').value
-  var txt1 = txt0.replace(/\s/g, '`')
-  // ^ This replace method is necessary for long texts
-  var txt = txt1.toUpperCase()
-  var txt23 = txt.slice(0, txt.length)
-  var txt33 = txt23.split('')
-  var txt2 = txt.replace(/~/g, '')
-  var txt3 = txt2.split('')
-  var txt4 = txt3.length
+<!DOCTYPE html>
+<html>
+	<head>
+	<script>
+  		function theTwistedStick (mode) {
+  			// Prepare text
+  			var plaintext = document.getElementById('textarea').value
+  			var spaceFiller = plaintext.replace(/\s/g, '`')
+  			// ^ This replace method is necessary for long texts
 
-  // Get variables and remove non-digit characters in PIN.
-  var pin = document.getElementById('wp').value
-  var pin2 = pin.replace(/\D/g, '')
-  var pin3 = pin2.split('')
-  var pin4 = pin3.map(function (x) {
-    return parseInt(x, 10)
-  })
-  var pin5 = Math.max.apply(null, pin4)
+  			var txt = spaceFiller.toUpperCase()
+  			var slicedText = txt.slice(0, txt.length)
+  			var ciphertext1 = slicedText.split('')
 
-  // PIN counter
-  var a = 0
-  var e = []
-  while (a < txt4) {
-    for (var b = 0; b < pin4.length; b++) {
-      var d = []
-      a += pin4[b]
-      // Split into PIN chunks
-      for (var c = 0; c < pin[b]; c++) {
-        d += txt3.shift()
-      }
-      e.push(d)
-    }
-  }
-  // Get rid of excess
-  var e1 = e.map(function (x) {
-    return x.replace(/undefined/g, '')
-  })
-  var e2 = e1.filter(function (x) {
-    return x !== ''
-  })
-  var e3 = e2.slice(0, e2.length)
-  // Fill w/ 0s
-  for (var l = 0; l < e2.length; l++) {
-    while (e2[l].length < pin5) {
-      e2[l] += '~'
-    }
-  }
-  // Arrange
-  var f = []
-  for (var k = 0; k < e2.length; k++) {
-    // Break up chunks
-    f.push(e2[k].split(''))
-  }
-  var g = []
-  // Max array length is 9
-  for (var i = 0; i < 9; i++) {
-    // For each array
-    for (var j = 0; j < f.length; j++) {
-      // Access & move array items within another array
-      var h = f[j]
-      g.push(h[i])
-    }
-  }
-  var g1 = g.toString()
-  var g2 = g1.replace(/,/g, '')
+  			var spaceReplacedText = txt.replace(/~/g, '')
+  			var ciphertext2 = spaceReplacedText.split('')
+  			var ciphertext2Length = ciphertext2.length
 
-  // Dearrange
-  var n = []
-  n.length = e3.length
-  while (txt33.length !== 0) {
-    for (var m = 0; m < e3.length; m++) {
-      var o = txt33.shift()
-      n[m] += o
-    }
-  }
-  var n1 = n.toString()
-  var n2 = n1.replace(/,/g, '')
-  var n3 = n2.replace(/~/g, '')
-  var n4 = n3.replace(/undefined/g, '')
-  var n5 = n4.replace(/`/g, ' ')
+  			// Get variables and remove non-digit characters in PIN.
+  			var pin = document.getElementById('pin').value
+  			var digitizedPin = pin.replace(/\D/g, '')
+  			var pinArray = digitizedPin.split('')
+  			var decimalChecked = pinArray.map(function (x) {return parseInt(x, 10)})
+  			var maxPinNumber = Math.max.apply(null, decimalChecked)
 
-  if (mode === 'enc') {
-    document.getElementById('p').innerHTML = g2
-  } else if (mode === 'dec') {
-    document.getElementById('p').innerHTML = n5
-  }
-}
+  			// PIN counter
+  			var pinCounter = 0
+  			var ciphertextArray = []
+  			while (pinCounter < ciphertext2Length) {
+    			for (let i = 0; i < decimalChecked.length; i++) {
+      				var d = []
+      				pinCounter += decimalChecked[i]
+      				// Split into PIN chunks
+      				for (let j = 0; j < pin[i]; j++) {
+        				d += ciphertext2.shift()
+      				}
+      				ciphertextArray.push(d)
+    			}
+  			}
+  			// Get rid of excess
+  			var ciphertextArray1 = ciphertextArray.map(function (x) {return x.replace(/undefined/g, '')})
+  			var ciphertextArray2 = ciphertextArray1.filter(function (x) {return (x !== '')})
+  			var ciphertextArrayClone = ciphertextArray2.slice(0, ciphertextArray2.length)
+  			// Fill w/ 0s
+  			for (let) i = 0; i < ciphertextArray2.length; i++) {
+    			while (ciphertextArray2[i].length < maxPinNumber) {
+      				ciphertextArray2[i] += '~'
+    			}
+  			}
+  			// Arrange
+  			var f = []
+  			for (let i = 0; i < ciphertextArray2.length; i++) {
+    			// Break up chunks
+    			f.push(ciphertextArray2[i].split(''))
+  			}
+  			var g = []
+  			// Max array length is 9
+  			for (let i = 0; i < 9; i++) {
+    			// For each array
+    			for (let j = 0; j < f.length; j++) {
+      				// Access & move array items within another array
+      				var h = f[j]
+      				g.push(h[i])
+    			}
+  			}
+  			var ciphertextString1 = g.toString()
+  			var ciphertextResult = ciphertextString1.replace(/,/g, '')
+
+  			// Dearrange
+  			var plaintextArray = []
+  			plaintextArray.length = ciphertextArrayClone.length
+  			while (ciphertext1.length !== 0) {
+    			for (let i = 0; i < ciphertextArrayClone.length; i++) {
+      				var plaintextItem = ciphertext1.shift()
+      				plaintextArray[i] += plaintextItem
+    			}
+  			}
+  			var plaintextString = plaintextArray.toString()
+  			var plaintextString2 = plaintextString1.replace(/,/g, '')
+        var plaintextString3 = plaintextString2.replace(/~/g, '')
+  			var plaintextString4 = plaintextString3.replace(/undefined/g, '')
+  			var plaintextResult = plaintextString4.replace(/`/g, ' ')
+
+  			if (mode === 'encrypt') {
+    			document.getElementById('p').innerHTML = ciphertextResult
+  			} else if (mode === 'decrypt') {
+    			document.getElementById('p').innerHTML = plaintextResult
+  			}
+		}
+	</script>
+  </head>
+  <body>
+    <style>
+    	p {
+        	width: 500px;
+          word-wrap: break-word;
+        }
+    </style>
+    <script src="tts.js"></script>
+    <p><textarea id="textarea" rows="8" cols="80">Send more troops to southern flankand</textarea></p>
+    <p>
+      PIN
+      <input id="pin" type="text"></input>
+    </p>
+    <button onclick="theTwistedStick('encrypt')">Encrypt</button>
+    <button onclick="theTwistedStick('decrypt')">Decrypt</button>
+    <p id='p'></p>
+  </body>
+</html>
